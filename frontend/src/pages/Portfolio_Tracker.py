6eb027ce315c6c5f51c1a4830c51c1ab945c49f6
Dai En <tohdaien01@gmail.com>
2026-06-11 23:47:26 +0800

    fixed up pie chart and started building of dataframe for portolio graph

--- frontend/src/pages/Portfolio_Tracker.py
+++ frontend/src/pages/Portfolio_Tracker.py
@@ -6,6 +6,8 @@ from datetime import datetime , date
 from alpha_vantage.timeseries import TimeSeries
 import os
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import pandas as pd
 
 load_dotenv()
 alpha_key = os.getenv("ALPHA_VANTAGE_KEY")
@@ -115,13 +117,19 @@ else:
     top_container = st.container()
     tcol1,tcol2,tcol3,tcol4 = top_container.columns(4,border=True)
     
-#calculation for stock data
+#calculation for portfolio data
     personal_stock_data = data[st.session_state.user_name]["stock_data"]
     total_stock_value = 0
     total_profit = 0
     total_amount_invested = 0
     total_number_of_stocks_held = 0
+    personal_stock_data_values = []
+    personal_stock_data_labels = []
+    personal_stock_data_gains = []
+    graph_data = {}
     for stocks in personal_stock_data:
+        x_stock_value = 0
+        x_stock_gain = 0
         ticker_data,meta_data = get_ticker_data(stocks)
         latest_price = ticker_data["4. close"].values[0]
         for key, values in personal_stock_data[stocks].items():
@@ -131,24 +139,41 @@ else:
             total_profit += ((latest_price-price)*amount)
             total_amount_invested += (amount * price)
             total_number_of_stocks_held += amount
-        
-#Total stock price
+            x_stock_value += float(amount*latest_price)
+            x_stock_gain += ((latest_price-price)*amount)
+        personal_stock_data_values.append(x_stock_value)
+        personal_stock_data_labels.append(stocks)
+        personal_stock_data_gains.append(x_stock_gain)
+    graph_data ={
+        "stock_name": personal_stock_data_labels,
+        "allocation": personal_stock_data_values,
+        "gains": personal_stock_data_gains
+    }
+    
+    graph_df = pd.DataFrame(graph_data)
+    
     with tcol1:
         st.write(f"Total Stock Value: ${total_stock_value:.2f}")
-            
-#Total stock profits
     with tcol2:
-        st.write(f"Total Profit: ${total_profit:.2f}")
-        
+        st.write(f"Total Profits: ${total_profit:.2f}")
     with tcol3:
         st.write(f"Total Amount Invested: ${total_amount_invested:.2f}")
     with tcol4:
         st.write(f"Number of Stocks Held: {int(total_number_of_stocks_held)}")
+        
+    
     bottom_container = st.container()
     bcol1,bcol2 = bottom_container.columns(2,border=True)
     with bcol1:
         st.write("Portfolio Pie Chart")
+        fig, ax = plt.subplots()
+        cmap_colors = plt.cm.Pastel2.colors 
+        ax.pie(personal_stock_data_values, labels=personal_stock_data_labels, autopct='%1.1f%%', startangle=90,colors=cmap_colors)
+        ax.axis('equal')
+        fig.patch.set_facecolor('white')
+        st.pyplot(fig)
+        
     with bcol2:
         st.write("Portfolio information: Value/Invested, Gains, Allocation")
-        
+        st.dataframe(graph_df)
     
